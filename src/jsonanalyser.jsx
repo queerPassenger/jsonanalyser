687dd9e0c5046bbeaf2703a889ae036a9fced30d
allanvalooran <allanmvalooran@gmail.com>
2018-07-29 18:10:19 +0530

    designInProgress

--- src/jsonanalyser.jsx
+++ src/jsonanalyser.jsx
@@ -4,18 +4,21 @@ class JSONAnalyser extends React.Component{
     constructor(props){
         super(props);
         this.style={
-            minHeight:screen.availHeight-120-16-50
+            minHeight:window.innerHeight-124-20,
         }
     }
     
     render(){
         return(
             <div className="jsonanalyser_container" style={this.style}>
-                <div className="lhs_container">
+                <div className="lhs_container" contenteditable={"true"} style={this.style}>
                 </div>
-                <div className="cs_container">
+                <div className="cs_container" style={this.style}>
+                    <div className="right_arrow_container">
+                        <img src="img/rightArrow.png"></img>
+                    </div>
                 </div>
-                <div className="rhs_container">
+                <div className="rhs_container" contenteditable={"true"} style={this.style}>
                 </div>
             </div>
         )
